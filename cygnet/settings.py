69c621e9ae5bc48232f6ec93a86d4cdd983415c6
Joseph.Ballard <joseph.ballard@tiga.us>
2019-07-22 12:16:52 -0500

    sql server config

--- cygnet/settings.py
+++ cygnet/settings.py
@@ -86,11 +86,11 @@ DATABASES = {
         'ENGINE': 'sql_server.pyodbc',
         'NAME': 'cygnet-util',
         'USER': 'jpb8@encore-db',
-        'PASSWORD': "Bowser33",
+        'PASSWORD': 'Bowser33',
         'HOST': 'encore-db.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server',
+            'driver': 'SQL Server Native Client 11.0',
         },
     },
 }
