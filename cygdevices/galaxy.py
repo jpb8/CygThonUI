2aa9e16e875cb0abbde275ba310d1d28a8fda8cd
Joseph.Ballard <joseph.ballard@tiga.us>
2020-03-04 10:14:59 -0600

    add scaling to end of export

--- cygdevices/galaxy.py
+++ cygdevices/galaxy.py
@@ -13,25 +13,60 @@ def no_commas(string):
             quotes = False
         if quotes == False:
             output += char
-        if char != ',' and quotes == True:
+        if char != ',' and quotes:
             output += char
-        elif char == "," and quotes == True:
+        elif char == "," and quotes:
             output += "*"
     return output
+
+
+
+def parse_scaling(df_scale, new):
+    if len(new.columns) == 2:
+        return df_scale
+    attrs = []
+    for col in new.columns:
+        split = col.split(".")
+        if len(split) > 1 and split[0] not in attrs:
+            attrs.append(split[0])
+    if len(attrs) < 1:
+        new["attr"] = new[":Tagname"]
+        new.columns = [":Tagname", "Area", "RawMin", "RawMax", "EngUnitsMin", "EngUnitsMax", "attr"]
+        df_scale = df_scale.append(new, ignore_index=True, sort=False)
+    else:
+        rows = new.shape[0]
+        for attr in attrs:
+            if attr + ".RawMin" in new.columns and attr + ".EngUnitsMin" in new.columns:
+                if attr + ".RawMin" not in new.columns:
+                    cols = [":Tagname", "Area", attr + ".EngUnitsMin", attr + ".EngUnitsMax", attr + ".EngUnitsMin", attr + ".EngUnitsMax"]
+                elif attr + ".EngUnitsMin" not in new.columns:
+                    cols = [":Tagname", "Area", attr + ".RawMin", attr + ".RawMax", attr + ".RawMin", attr + ".RawMax"]
+                else:
+                    cols = [":Tagname", "Area", attr + ".RawMin", attr + ".RawMax", attr + ".EngUnitsMin", attr + ".EngUnitsMax"]
+                temp = new[cols]
+                temp["attr"] = [attr for i in range(rows)]
+                temp.columns = [":Tagname", "Area", "RawMin", "RawMax", "EngUnitsMin", "EngUnitsMax", "attr"]
+                df_scale = df_scale.append(temp, ignore_index=True, sort=False)
+    return df_scale
 
 
 def create_xlxs(templates):
     sio = BytesIO()
     writer = pd.ExcelWriter(sio, engine="xlsxwriter")
+    df_scalers = pd.DataFrame(columns=[":Tagname", "Area", "RawMin", "RawMax", "EngUnitsMin", "EngUnitsMax", "attr"])
     for k, v in templates.items():
         df = pd.DataFrame([sub.split(",") for sub in v])
         df.columns = df.iloc[0]
         df = df[1:]
         reg_cols = [col for col in df if str(col).startswith('reg') or str(col).startswith('Reg')]
         bit_col = [col for col in df if str(col).startswith('bit') or str(col).startswith('Bit')]
+        eng_units = [col for col in df if 'EngUnitsM' in str(col) or 'RawM' in str(col) or 'EngUnitsM' in str(col)]
+        df_eng = df[list(df.columns[0:2]) + eng_units]
+        df_scalers = parse_scaling(df_scalers, df_eng)
         source_col = [col for col in df if str(col).endswith('InputSource') or str(col).endswith('OutputDest')]
-        df = df[list(df.columns[0:4]) + reg_cols + bit_col + source_col]
+        df = df[list(df.columns[0:4]) + reg_cols + bit_col + source_col + eng_units]
         df.to_excel(writer, sheet_name=k, index=False)
+    df_scalers.to_excel(writer, sheet_name="scales", index=False)
     writer.save()
     writer.close()
     sio.seek(0)
